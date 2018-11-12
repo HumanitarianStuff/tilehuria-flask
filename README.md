4a2b7cdc44278963df740001584685d9f5c51fd8
Ivan Gayton <ivangayton@gmail.com>
2018-11-12 11:00:44 +0000

    More or less working setup; still needs download links and wsgi

---
+++ README.md
@@ -0,0 +1,11 @@
+# Server for TileHuria
+
+## Installation
+
+Some stuff
+
+install GDAL
+
+https://stackoverflow.com/questions/32066828/install-gdal-in-virtualenvwrapper-environment
+
+pip install pillow
--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,43 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.utils import secure_filename
+import sys, os
 from app import app
-from app.forms import LoginForm
+from app.tilehuria.tilehuria.polygon2mbtiles import polygon2mbtiles
+
+def scandir(dir): 
+    """Walk recursively through a directory and return a list of all files in it"""
+    filelist = []
+    for path, dirs, files in os.walk(dir):
+        for f in files:
+            filelist.append(os.path.join(path, f))
+    return filelist
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember me={}'.format(
-              form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['polygon']
+        filename = secure_filename(file.filename)
+        pathname = (os.path.join('files', filename))
+        file.save(pathname)
+        opts = {}
+        opts ['infile'] = pathname
+        polygon2mbtiles(opts)
+        return render_template('upload.html', uploaded_file=filename)
+    else:
+        return render_template('index.html', title='No file. Try again!')
+
+@app.route('/mbtiles')
+def mbtiles():
+    all_files = scandir('files')
+    tilesets = []
+    for filename in all_files:
+        (basename, extension) = os.path.splitext(filename)
+        if extension == '.mbtiles':
+            tilesets.append(filename)
+    return render_template('mbtiles.html', title='MBTiles for download',
+                           tilesets=tilesets)
